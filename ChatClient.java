3819a139a03631ff4248fb5977d4693865412209
ianvictor <ianvictorandrade@hotmail.com>
2023-11-01 10:33:25 -0400

    cliente consegue enviar mensagens ao servidor

--- ChatClient.java
+++ ChatClient.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -6,6 +7,7 @@ public class ChatClient {
     private final String SERVER_ADRESS = "127.0.0.1";
     private Socket clientSocket;
     private Scanner scanner;
+    private PrintWriter out;
 
     public ChatClient() {
         scanner = new Scanner(System.in);
@@ -23,16 +25,18 @@ public class ChatClient {
 
     public void start() throws IOException {
         clientSocket = new Socket(SERVER_ADRESS, ChatServer.PORT);
+        this.out = new PrintWriter(clientSocket.getOutputStream(), true);
         System.out.println("Cliente conectado ao servidor " + SERVER_ADRESS + ":" + ChatServer.PORT);
         messageLoop();
     }
 
-    private void messageLoop() {
+    private void messageLoop() throws IOException {
         String message = null;
 
         do {
             System.out.print("Digite uma mensagem (ou sair para finalizar): ");
             message = scanner.nextLine();
+            out.println(message);
         } while (!message.equalsIgnoreCase("sair"));
 
     }
--- ChatServer.java
+++ ChatServer.java
@@ -1,18 +1,19 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
-        public static final int PORT = 4000;
-        private ServerSocket serverSocket;
+    public static final int PORT = 4000;
+    private ServerSocket serverSocket;
 
-
-        public static void main(String[] args) {
+    public static void main(String[] args) {
             ChatServer server = new ChatServer();
             try {
                 server.start();
             } catch (IOException e) {
-                System.out.println("Erro ao startar o servidor: " + e.getMessage());
+                System.out.println("Erro ao iniciar o servidor: " + e.getMessage());
             }
 
             System.out.println("Servidor finalizado");
@@ -28,6 +29,9 @@ public class ChatServer {
         while (true) {
             Socket clientSocket = serverSocket.accept();
             System.out.println("Cliente conectado com IP: " + clientSocket.getRemoteSocketAddress());
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String message = in.readLine();
+            System.out.println("Mensagem recebida: " + clientSocket.getRemoteSocketAddress() + ":" + message);
         }
     }
 }
